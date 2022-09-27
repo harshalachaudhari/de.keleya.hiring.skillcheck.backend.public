ec3711f46953a9dd0cc90885edb75edfbf1e2658
061193 <hchaudhari307@gmail.com>
2022-09-27 15:49:53 +0200

    Added unencrypted password valiadation logic

--- src/common/validators/unencrypted-password-validator.ts
+++ src/common/validators/unencrypted-password-validator.ts
@@ -6,16 +6,66 @@ export type UnencryptedPassword = string;
 @ValidatorConstraint({ name: 'unencryptedPasswordValidator', async: true })
 @Injectable()
 export class UnencryptedPasswordValidator implements ValidatorConstraintInterface {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  async validate(val: UnencryptedPassword, args: ValidationArguments) {
+  async validate(val: UnencryptedPassword, args: any) {
+
     if (!val) return false;
-    return this.isValidPassword(val);
+    return this.isValidPassword(val, args.constraints['length'], args.constraints['patternsToEscape'], args.constraints['caseSensitivty'], args.constraints['numericDigits'], args.constraints['specialChars']);
   }
 
-  isValidPassword(passwordToValidate: string): boolean {
+  isValidPassword(
+    passwordToValidate: string,
+    length: number,
+    patternsToEscape: [string],
+    caseSensitivty: boolean,
+    numericDigits: boolean,
+    specialChars: boolean
+  ): boolean {
+
     if (!passwordToValidate) return false;
-    // TODO: implement validation rules
-    if (passwordToValidate.length < 6) return false;
+    if (passwordToValidate.length < length) {
+
+      return false;
+    }
+    if (caseSensitivty) {
+
+      const hasUpperCase = /[A-Z]/.test(passwordToValidate);
+      if (!hasUpperCase) {
+
+        return false;
+      }
+      const hasLowerCase = /[a-z]/.test(passwordToValidate);
+      if (!hasLowerCase) {
+
+        return false;
+      }
+    }
+    if (numericDigits) {
+
+      const hasNumbers = /[\d]/.test(passwordToValidate);
+      if (!hasNumbers) {
+        return false;
+      }
+    }
+    if (specialChars) {
+
+      const hasSpeacialChars = /[[@$!%*#?&\]]/.test(passwordToValidate);
+
+      if (!hasSpeacialChars) {
+        return false;
+      }
+    }
+    if (patternsToEscape.length > 0) {
+
+      const passwordToValidateLowerCase = passwordToValidate.toLowerCase();
+      for (const pattern in patternsToEscape) {
+        const hasMatchesWithPattern = passwordToValidateLowerCase.match(new RegExp(pattern)).length;
+
+        if (hasMatchesWithPattern) {
+          return false;
+        }
+      }
+    }
+
     return true;
   }
 
